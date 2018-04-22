c4d417d5bf3180e6ad2012cb56ffad77a76c009c
Hoffmann Ági <entonibaba0720@gmail.com>
2018-04-22 16:27:22 +0200

    current date/time

--- Javascript exercises/currenttime/currenttime.js
+++ Javascript exercises/currenttime/currenttime.js
@@ -1,6 +1,15 @@
 /*
 Write a JavaScript program to display the current day and time in the following format.
+Sample Output : Today is : Friday. 
+Current time is : 4 PM : 50 : 22
 */
 
-var date = new Date();
-document.getElementById("date").innerHTML = date.toString();
+var today = new Date();
+var day = today.getDay();
+var days = ["Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"];
+console.log("Ma " + days[day] + " van!");
+var hours = today.getHours();
+var minutes = today.getMinutes();
+var seconds = today.getSeconds();
+console.log("Aktuális idő " + hours + " : " + minutes + " : " + seconds );
+
