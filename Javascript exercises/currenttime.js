fe33bd11dcfca1d07b36acef8bf287e58748f1e9
Hoffmann Ági <entonibaba0720@gmail.com>
2018-04-22 14:49:16 +0200

    current time

---
+++ Javascript exercises/currenttime.js
@@ -0,0 +1,6 @@
+/*
+Write a JavaScript program to display the current day and time in the following format.
+*/
+
+var date = new Date();
+document.getElementById("date").innerHTML = date.toString();
