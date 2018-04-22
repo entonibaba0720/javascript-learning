1e5d03b6a6a41e8de28078b5318c4a4bcf53f40f
Hoffmann Ági <entonibaba0720@gmail.com>
2018-04-22 09:25:32 +0200

    Welcome User

---
+++ Javascript exercises on String - Welcome User/app.js
@@ -0,0 +1,9 @@
+/*
+Javascript exercises on String : Welcome User
+Get the first name and last name from the user using the prompt box and display a welcome message that says “Hello Parvez Ansari”
+*/
+"use strict";
+
+   
+   var firstName = prompt("What is your first name?");
+    alert ("Hello " + firstName);
