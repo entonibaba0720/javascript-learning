a4007391079d1bce4ab632cf9b5d32b8240cd72d
entonibaba0720 <entonibaba0720@gmail.com>
2018-04-22 16:12:06 +0200

    Delete currenttime.js

--- Javascript exercises/currenttime/currenttime.js
+++
@@ -1,6 +0,0 @@
-/*
-Write a JavaScript program to display the current day and time in the following format.
-*/
-
-var date = new Date();
-document.getElementById("date").innerHTML = date.toString();
